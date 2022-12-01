16c12dd060ab528d4a76c182b2d7aa631f722b7c
syed zubair <zubairsyed0301@gmail.com>
2022-12-02 02:33:50 +0530

    done with arr_rev 4 methods using unshfit, forEach,reverse(),swapping, for loop starting index as arr.length-1;>=0;i--

---
+++ arrayRev.js
@@ -0,0 +1,47 @@
+// function Arr(arr,start,end){
+    let arr=[1,2,3,4,5,6]
+    // console.log(arr)
+    let temp=0,x=arr.length;
+    let start=0,end=arr.length-1;
+    while(start<end){
+        console.log("temp",temp)
+        temp=arr[start]
+        arr[start]=arr[end]
+        arr[end]=temp
+        start++
+        end--
+    }
+    // return arr
+// }
+
+
+// console.log(Arr(arr,0,arr.length-1))
+console.log(arr)
+
+// method 2 unshift (acts as prepend)
+let arrUn=[1,2,3,4,5,6]
+let arrRevUn = [];
+arrUn.forEach(function (item){
+    arrRevUn.unshift(item)
+})
+
+console.log(arrRevUn)
+
+
+// method 3
+let arrFor=[1,2,3,4,5,6]
+let arrForNew = []
+for(let i=arrFor.length-1 ; i>=0;i--){
+    arrForNew.push(arrFor[i])
+}
+
+console.log(arrForNew)
+
+// method 4 reduce
+// Using reduce() method: In this approach we we reduce function which apply callback function on each element and get summarized result of all item in accumulator. First, we will use reduce method on array and take empty as accumulator and append each element at the beginning of array. At the end we get the reverse of original array. 
+
+let x1 = [1,2,3,4,5,6]
+let z=0
+x1.reduce((accumulator, val) =>{
+    [val].concat(accumulator)
+},[] )
--- fibonacci.js
+++ fibonacci.js
@@ -1,8 +1,8 @@
 //fibbonacci using  recursion 
 // two major points in recursion
 // 1. (fibbonacci) recursion works on calling itself till it meets some condition and 
-//      starts adding value from the point where its condition fails now call back starts in 
-//      stack memory.
+//      starts adding value from the point where its condition fails now function call back starts in 
+//      stack memory, every function callback contain some value in it basically a return type.
 // 2. it changes the value n at the same time
 
                 // factorial                           fibonacci
@@ -22,26 +22,84 @@
 
 
 
-function fib(n){
-    if(n==0){
-        return 0
-    }else if(n==1){
-        return 1;
-    }
-    console.log("fib n",n)
-    return fib(n-1);
-}
+// // function fib(n){
+// //     if(n==0){
+// //         return 0
+// //     }else if(n==1){
+// //         return 1;
+// //     }
+// //     console.log(n)
+// //     return fib(n-1)+fib(n-2)
+// // }
 
-// fib(n-1) will call itself and also changes the value of n at same time 
+// // // fib(n-1) will call itself and also changes the value of n at same time 
 
-console.log(fib(9))
+// // console.log(fib(9))
 
-// factorial using recursion
-function fact(n){
-    if(n==0 || n==1){
-        return 1;
-    } 
-    return n+fact(n-2)
-}
+// // // factorial using recursion
+// // function fact(n){
+// //     if(n==0 || n==1){
+// //         return 1;
+// //     } 
+// //     return n+fact(n-2)
+// // }
 
-console.log("fact",fact(9))
+// // console.log("fact",fact(9))
+
+
+// // prime number
+// let i=2;
+// let count=0;
+// function prime(n){
+//     let z=n
+//     if(z%n==0 && n<=5){
+//         count++;
+//     }
+//     if(count>=1){
+//         return -1
+//     }
+//     console.log(count)
+//     return prime(n-1);
+// }
+
+// prime(5)
+// // if(x==-1){
+// //     console.log("not a prime number ", x)
+// // }else{
+// //     console.log("a prime number ", x)
+// // }
+
+
+
+// JavaScript program to find whether a Number
+// is Prime or Not using Recursion
+
+	// Returns true if n is prime, else
+	// return false.
+	// i is current divisor to check.
+	function isPrime(n, i)
+	{
+		// Base cases
+		if (n <= 2)
+			return (n == 2) ? true : false;
+		if (n % i == 0){
+			return false;
+        }
+		if (i * i > n){
+			return true;
+        }
+            console.log(n,i);
+		// Check for next divisor
+		return isPrime(n, i + 1);
+	}
+
+
+// Driver code
+
+		
+	let n = 911;
+
+		if (isPrime(n, 2))
+			console.log("Yes");
+		else
+			console.log("No");
